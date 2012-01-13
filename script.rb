8edacfafafe37abcf3381eb3c0baad01ad84417b
Jon Tomas Gretarsson <jon.gretarsson@gmail.com>
2012-01-13 10:25:20 -0800

    Changing input format and adding colors
    - Permit the specification of colors on a per-task basis
    - Changed the delimiter from ',' to ';'

--- script.rb
+++ script.rb
@@ -9,18 +9,19 @@ overlay = QuickMagick::Image.solid(image.width,image.height)
 
 overlay.append_to_operators("transparent","white")
 overlay.append_to_operators("stroke","none")
-overlay.append_to_operators("fill","rgba(135, 206, 235,.5)")
 IO.foreach(ARGV[0]) {|line|
-    parsed=line.split(",").map{|tmp| tmp.strip}
+    parsed=line.split(";").map{|tmp| tmp.strip}
     start_arc_time = Float(parsed[0])
     end_arc_time   = Float(parsed[1])
-    outer_radius   = (start_arc_time.modulo(90.0) / 30.0).floor
-    box = [107.0 - radius[outer_radius],102.0 - radius[outer_radius],107.0 + radius[outer_radius],102.0 + radius[outer_radius]]
+    rgba_string = parsed[2]
+    overlay.append_to_operators("fill",rgba_string)
+    outer_radius   = (start_arc_time.modulo(90) / 30).floor
+    box = [107 - radius[outer_radius],102 - radius[outer_radius],107 + radius[outer_radius],102 + radius[outer_radius]]
     cone = [107, 102,
-           (107+(radius[outer_radius]-0.7)*Math.cos(Math::PI/180*start_arc_time)),
-           (102+(radius[outer_radius]-0.7)*Math.sin(Math::PI/180*start_arc_time)),
-           (107+(radius[outer_radius]-0.7)*Math.cos(Math::PI/180*end_arc_time)),
-           (102+(radius[outer_radius]-0.7)*Math.sin(Math::PI/180*end_arc_time))]
+           (107+(radius[outer_radius]-0.5)*Math.cos(Math::PI/180*start_arc_time)),
+           (102+(radius[outer_radius]-0.5)*Math.sin(Math::PI/180*start_arc_time)),
+           (107+(radius[outer_radius]-0.5)*Math.cos(Math::PI/180*end_arc_time)),
+           (102+(radius[outer_radius]-0.5)*Math.sin(Math::PI/180*end_arc_time))]
 
     overlay.draw_arc(box[0],box[1],box[2],box[3],start_arc_time,end_arc_time)
     overlay.draw_polygon(cone)
