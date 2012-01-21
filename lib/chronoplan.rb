2c9208a058f258b972abdff07dfb46f920cc97e0
Jon Tomas Gretarsson <jon.gretarsson@gmail.com>
2012-01-20 17:10:48 -0800

    Moving template files into their own folder
     - Moving template files into templates/
     - Added a new template style, borders-only

--- lib/chronoplan.rb
+++ lib/chronoplan.rb
@@ -9,28 +9,8 @@ class Chronoplan
         CENTERY=102
         INNER_RADIUS = RADIUS[-1]
 	ROOT = File.expand_path(File.join(File.dirname(__FILE__), "..")) # Get the root directory
-        TEMPLATE = %q{
-            push graphic-context
-                stroke 'none'
-                fill '<%= rgba_string %>'
-                path 'M <%= CENTERX %> <%= CENTERY %>
-                      m <%=  INNER_RADIUS * Math.cos(start_arc_time) %>
-                        <%= -INNER_RADIUS * Math.sin(start_arc_time) %>
-                      l <%=  (outer_radius-INNER_RADIUS) * Math.cos(start_arc_time) %>
-                        <%= -(outer_radius-INNER_RADIUS) * Math.sin(start_arc_time) %>
-                      a <%= outer_radius %>,<%= outer_radius %> 0
-                        <%= (start_arc_time - end_arc_time < Math::PI) ? '0' : '1' %>,1
-                        <%= outer_radius * (Math.cos(end_arc_time) - Math.cos(start_arc_time)) %>,
-                        <%= -outer_radius * (Math.sin(end_arc_time) - Math.sin(start_arc_time)) %>
-                      l <%=  (INNER_RADIUS-outer_radius) * Math.cos(end_arc_time) %>
-                        <%= -(INNER_RADIUS-outer_radius) * Math.sin(end_arc_time) %>
-                      a <%= INNER_RADIUS %>,<%= INNER_RADIUS %> 0
-                        <%= (start_arc_time - end_arc_time < Math::PI) ? '0' : '1' %>,0
-                        <%=  INNER_RADIUS * (Math.cos(start_arc_time) - Math.cos(end_arc_time)) %>,
-                        <%= -INNER_RADIUS * (Math.sin(start_arc_time) - Math.sin(end_arc_time)) %>
-                      z'
-            pop graphic-context
-        }
+        BASIC_ARC = ERB.new File.new("#{ROOT}/templates/basic_arc.erb").read
+        BORDERS_ARC = ERB.new File.new("#{ROOT}/templates/border_arc.erb").read
 
 	def initialize(input_file)
 		@input_file = input_file	
@@ -42,7 +22,6 @@ class Chronoplan
 
                 puts "push graphic-context"
                 puts "viewbox 0 0 214 204"
-                templ = ERB.new(TEMPLATE)
 		File.foreach(@input_file) do |line|
 			parsed = line.split(";").map { |tmp| tmp.strip }
 			start_arc_time = -Math::PI / 180 * parsed[0].to_f
@@ -51,7 +30,12 @@ class Chronoplan
 
 			outer_radius_index = (parsed[0].to_f.modulo(90) / 30).floor
                         outer_radius = RADIUS[outer_radius_index]
-                        puts templ.result(binding)
+                        #puts BASIC_ARC.result(binding)
+                        rgba_start_line = 'none'
+                        rgba_end_line = rgba_string
+                        rgba_inside_arc = 'none'
+                        rgba_outside_arc = rgba_string
+                        puts BORDERS_ARC.result(binding)
 		end
                 puts "pop graphic-context"
 	end
