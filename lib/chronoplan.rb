39dce2025f02d78559a079a106f0e6281ce8d9d6
Jon Tomas Gretarsson <jon.gretarsson@gmail.com>
2012-03-18 19:23:50 -0700

    Updating the parser
    - parser now understands time rather than the confusing
      'angle' nonsense.
    - removed the _overlay option.
    - fixed the example files.

--- lib/chronoplan.rb
+++ lib/chronoplan.rb
@@ -5,6 +5,8 @@ require 'quick_magick'
 
 class Chronoplan
 	# Constants
+        NINE_AM = Math::PI
+        NINE_PM = 3.*Math::PI
 	RADIUS = { 0 => 108, 1 => 144, 2 => 180, 3 => 188, -1 => 72, -2 => 48 }
         CENTERX=190
         CENTERY=190
@@ -18,7 +20,7 @@ class Chronoplan
 		@input_file = input_file	
 	end
 
-	def process
+        def parse
                 temp_file = Tempfile.new(['ScheduleOverlay','.mvg'])
                 puts temp_file.path
                 temp_file.write File.new("#{ROOT}/images/Chronodex-v2.mvg").read
@@ -27,22 +29,25 @@ class Chronoplan
                 temp_file.write "viewbox 0 0 380 380\n"
 		File.foreach(@input_file) do |line|
 			parsed = line.split(";").map { |tmp| tmp.strip }
-			start_arc_time = -Math::PI / 180 * (parsed[0].to_f-180)
-			end_arc_time   = -Math::PI / 180 * (parsed[1].to_f-180)
+			start_arc_time = time_to_radius(parsed[0])
+			end_arc_time   = time_to_radius(parsed[1])
 			rgba_string = parsed[2]
 
-			outer_radius_index = (parsed[0].to_f.modulo(90) / 30).floor
-                        outer_radius_index = -2 if parsed[0].to_f < 0
-                        outer_radius_index = 3 if parsed[0].to_f >= 360
+			outer_radius_index = radial_index(parsed[0])
+                        outer_radius_index = -2 if start_arc_time < NINE_AM
+                        outer_radius_index = 3 if start_arc_time >= NINE_PM
                         outer_radius = RADIUS[outer_radius_index]
+                        outer_radius = RADIUS[-1]+20 if start_arc_time < NINE_PM
+                        outer_radius = RADIUS[-1]-20 if start_arc_time < NINE_AM
+                        outer_radius = RADIUS[3]-1 if start_arc_time >= NINE_PM
 
-                        temp_file.write INTERIOR_ARC.result(binding) if parsed[0].to_f < 360
-                        temp_file.write OUTSIDE_ARC.result(binding) if parsed[0].to_f >= 360
+                        temp_file.write INTERIOR_ARC.result(binding) if start_arc_time < NINE_PM
+                        temp_file.write OUTSIDE_ARC.result(binding) if start_arc_time >= NINE_PM
                         rgba_start_line = rgba_string
                         rgba_end_line = rgba_string
                         rgba_inside_arc = rgba_string
                         rgba_outside_arc = rgba_string
-                        temp_file.write BORDERS_ARC.result(binding) if parsed[0].to_f < 360
+                        #temp_file.write BORDERS_ARC.result(binding) if start_arc_time < NINE_PM
 		end
                 temp_file.write "pop graphic-context\n"
                 temp_file.close
@@ -53,4 +58,53 @@ class Chronoplan
                 image.convert("output.png")
                 temp_file.unlink
 	end
+
+	def process
+                temp_file = Tempfile.new(['ScheduleOverlay','.mvg'])
+                puts temp_file.path
+                temp_file.write File.new("#{ROOT}/images/Chronodex-v2.mvg").read
+
+                temp_file.write "push graphic-context\n"
+                temp_file.write "viewbox 0 0 380 380\n"
+		File.foreach(@input_file) do |line|
+			parsed = line.split(";").map { |tmp| tmp.strip }
+			start_arc_time = time_to_radius(parsed[0])
+			end_arc_time   = time_to_radius(parsed[1])
+			rgba_string = parsed[2]
+
+			outer_radius_index = radial_index(parsed[0])
+                        outer_radius_index = -2 if start_arc_time < NINE_AM
+                        outer_radius_index = 3 if start_arc_time >= NINE_PM
+                        outer_radius = RADIUS[outer_radius_index]
+                        outer_radius = RADIUS[-1]+20 if start_arc_time < NINE_PM
+                        outer_radius = RADIUS[-1]-20 if start_arc_time < NINE_AM
+                        outer_radius = RADIUS[3]-1 if start_arc_time >= NINE_PM
+
+                        temp_file.write INTERIOR_ARC.result(binding) if start_arc_time < NINE_PM
+                        temp_file.write OUTSIDE_ARC.result(binding) if start_arc_time >= NINE_PM
+                        rgba_start_line = rgba_string
+                        rgba_end_line = rgba_string
+                        rgba_inside_arc = rgba_string
+                        rgba_outside_arc = rgba_string
+                        #temp_file.write BORDERS_ARC.result(binding) if start_arc_time < NINE_PM
+		end
+                temp_file.write "pop graphic-context\n"
+                temp_file.close
+
+                image = QuickMagick::Image.read(temp_file.path).first
+                image.append_to_operators("background","none")
+                image.append_to_operators("density","720")
+                image.convert("output.png")
+                temp_file.unlink
+	end
+
+        def time_to_radius(time_string)
+            hms = time_string.split(":")
+            Math::PI/180*(hms[0].to_f*30.0+hms[1].to_f*0.5+hms[2].to_f*0.25/3.0 - 90.0)
+        end
+
+        def radial_index(time_string)
+            hms = time_string.split(":")
+            hms[0].to_i.modulo(3)
+        end
 end # class
